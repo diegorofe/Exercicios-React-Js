52f02ea5c4c3f661f0d6251b06e6f968639d5563
Diego <diegorf55@gmail.com>
2021-11-23 16:53:16 -0300

    create exercise components

---
+++ lorem-Ipsum/lorem_ipsum/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Hello from './components/hello.js'
+import LoremIpsum from './components/loremIpsum';
+
+function App() {
+  return (
+   <div>
+      <Hello/>
+      <LoremIpsum/>      
+   </div>
+  );
+}
+
+export default App;
---
+++ lorem-Ipsum/lorem_ipsum/src/components/loremIpsum.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class LoremIpsum extends React.Component {
+    render() {
+        return <p>Vou aprender MUIIIITO React-Js!</p>
+    }
+}
+
+export default LoremIpsum 

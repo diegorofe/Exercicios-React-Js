6cee2fe25b1b4b89351f55871295570583d0504d
Diego <diegorf55@gmail.com>
2021-11-27 09:54:42 -0300

    refactoring with hooks

--- lorem-Ipsum/lorem_ipsum/src/components/planets/index.js
+++ lorem-Ipsum/lorem_ipsum/src/components/planets/index.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 import Planet from "../planet";
 
 
@@ -8,46 +8,44 @@ async function getPlanets() {
      return data;
 }
 
-class Planets extends React.Component {
-     constructor(props){
-          super(props);
-          this.state = {
-               planets: [ ]
-          }
+// componentDidMount(){
+//      getPlanets().then(data => {
+//           .setState(state => ({
+//                planets: data['planets']
+//           }))
+//      })
+// }
+
+const Planets = () => {
+     const [planets, setPlanets] = useState([
+          {
+               "id": "mars",
+               "name": "Mars",
+               "description": "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System after Mercury. In English, Mars carries a name of the Roman god of war and is often referred to as the 'Red Planet'.",
+               "img_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/220px-OSIRIS_Mars_true_color.jpg",
+               "link": "https://en.wikipedia.org/wiki/Mars"
+             }
+     ])
+     const duplicateLast = () => {
+          let last_planet = planets[planets.length - 1];
+          setPlanets(...planets, last_planet);
      }
 
-     componentDidMount(){
-          getPlanets().then(data => {
-               this.setState(state => ({
-                    planets: data['planets']
-               }))
-          })
+     const removelast = () => {
+          let new_planets = [...planets];
+          new_planets.pop();
+          setPlanets(new_planets);
      }
 
-     duplicateLast = () => {
-          let last_planet = this.state.planets[this.state.planets.length - 1]
-          this.setState(state => ({
-               planets: [...this.state.planets, last_planet]
-          }))
-     }
-
-     removelast = () => {
-          let new_planets = [...this.state.planets]
-          new_planets.pop()
-          this.setState(state => ({
-               planets: new_planets
-          }))
-     }
-
-     render(){
+ 
           return (
                <Fragment> 
                     <h4>Planet List</h4>
                     <hr/>
-                    <button onClick={this.removelast}>Remove Last</button>
-                    <button onClick={this.duplicateLast}>Duplicate Last</button>
+                    <button onClick={removelast}>Remove Last</button>
+                    <button onClick={duplicateLast}>Duplicate Last</button>
                     <hr/>
-               {this.state.planets.map((planet, index) =>
+               {planets.map((planet, index) =>
                     
                     <Planet 
                          id ={planet.id}
@@ -64,6 +62,6 @@ class Planets extends React.Component {
           
                </Fragment>
                )
-          }
+          
 }
 export default Planets

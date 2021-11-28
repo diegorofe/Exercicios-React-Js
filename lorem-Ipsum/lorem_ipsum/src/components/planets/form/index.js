0a8cb13b56421a27164ff9b93aed6b42baccaabe
Diego <diegorf55@gmail.com>
2021-11-28 08:36:44 -0300

    exercise form with multiple inputs

--- lorem-Ipsum/lorem_ipsum/src/components/planets/form/index.js
+++ lorem-Ipsum/lorem_ipsum/src/components/planets/form/index.js
@@ -1,14 +1,26 @@
 import React, {Fragment, useState, useEffect} from "react";
 
 
+const initialState = {
+    name: '',
+    description: '',
+    link: '',
+    img_url: ''
+}
+
+
 const Form = (props) => {
-    const [name, setName] = useState('');
-    const handleChange = (event) => setName(event.target.value);
+    const [fields, setFields] = useState(initialState);
+
+    const handleFieldsChange = (event) => setFields({
+        ...fields,
+        [event.currentTarget.name]: event.currentTarget.value
+    });
     const handleSubmit = event => {
-        props.addPlanet({name: name})
-        
+        props.addPlanet(fields)
         //evita o recarregamento defaut
         event.preventDefault();
+        setFields(initialState)
 
     }
 
@@ -17,7 +29,19 @@ const Form = (props) => {
         <form onSubmit={handleSubmit}>
             <div>
                 <label htmlfor="name">Name: </label>
-                <input id="name" type="text" value={name} onChange={handleChange}/>
+                <input id="name" type="text" name="name" value={fields.name} onChange={handleFieldsChange}/>
+            </div>
+            <div>
+                <label htmlfor="description">Description: </label>
+                <input id="description" type="text" name="description" value={fields.description} onChange={handleFieldsChange}/>
+            </div>
+            <div>
+                <label htmlfor="link">Link: </label>
+                <input id="link" type="text" name="link" value={fields.link} onChange={handleFieldsChange}/>
+            </div>
+            <div>
+                <label htmlfor="img_url">Image URL: </label>
+                <input id="img_url" type="text" name="img_url" value={fields.img_url} onChange={handleFieldsChange}/>
             </div>
             <br/>
             <input type="submit"/>
